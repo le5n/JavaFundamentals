afa81ac9e23efa6295a87c11efd2c7d6f0746027
Lena <ellenageor@gmail.com>
2016-09-23 03:39:55 +0400

    Task 1 almost finished

--- src/main/java/homeworkThree/taskOne/CrazyLogger.java
+++ src/main/java/homeworkThree/taskOne/CrazyLogger.java
@@ -1,22 +1,44 @@
 package homeworkThree.taskOne;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 class CrazyLogger {
     private StringBuilder logger = new StringBuilder();
+    private String separation = "|";
+    //const length of formatted date&time
+    private final static int dateLength = 23;
 
 
     void makeNote(String note) {
-        LocalDateTime localDate = LocalDateTime.now();
-        logger.append(note).append(" ").append(localDate.format(DateTimeFormatter.ofPattern("dd-mm-YYYY : hh-mm")));
-
-
+        LocalDateTime localDate = LocalDateTime.of(LocalDate.now(), LocalTime.now());
+        logger.append(separation).
+                append(localDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy : hh - mm"))).
+                append(" - ").
+                append(note);
     }
 
     StringBuilder getLogger() {
         return logger;
     }
+
+    public char[] searchByDate(String date) {
+        int searchIndex = logger.indexOf(date);
+        int separationIndex = logger.indexOf(separation, searchIndex);
+
+        char[] searchResult = new char[separationIndex - searchIndex];
+
+        if (searchResult.length < separationIndex && searchResult.length > dateLength) {
+            logger.getChars(searchIndex, separationIndex, searchResult, 0);
+            System.out.println(searchResult);
+        } else {
+            System.out.println("Date not found");
+        }
+        return searchResult;
+    }
+
 }
 
 
