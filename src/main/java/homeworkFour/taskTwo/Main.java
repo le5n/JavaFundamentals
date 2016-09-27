152cb3d6e2ec43b6ccf48fe9a34a7f0a5df75f56
Lena <ellenageor@gmail.com>
2016-09-27 16:00:36 +0400

    task2 is almost finished

--- src/main/java/homeworkFour/taskTwo/Main.java
+++ src/main/java/homeworkFour/taskTwo/Main.java
@@ -5,7 +5,18 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         CharIO charIO = new CharIO();
-        charIO.searchWords("D:\\Программы\\JavaFundamentals\\src\\main\\java\\homeworkFour\\taskOne\\ByteIO.java");
+        StringBuilder resultText;
+
+        String fileName =
+                "D:\\Программы\\JavaFundamentals\\src\\main\\java\\homeworkFour\\taskOne\\ByteIO.java";
+        String keyFileName =
+                "D:\\Программы\\JavaFundamentals\\src\\main\\java\\homeworkFour\\taskOne\\keyWords.txt";
+        String outFileName =
+                "D:\\Программы\\JavaFundamentals\\src\\main\\java\\homeworkFour\\taskTwo\\result.txt";
+// TODO: 27.09.2016 solve problem with incorrect output
+        resultText = charIO.findKeyWords(fileName,keyFileName);
+        boolean whatHappened = charIO.writeInFile(resultText,outFileName);
+        System.out.println(whatHappened);
 
     }
 }
