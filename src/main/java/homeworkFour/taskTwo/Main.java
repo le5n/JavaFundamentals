149feed7fb04d184c1239a9911317a3d588b3bbb
Lena <ellenageor@gmail.com>
2016-09-27 15:03:56 +0400

    some changes + task2 in hw4

---
+++ src/main/java/homeworkFour/taskTwo/Main.java
@@ -0,0 +1,11 @@
+package homeworkFour.taskTwo;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        CharIO charIO = new CharIO();
+        charIO.searchWords("D:\\Программы\\JavaFundamentals\\src\\main\\java\\homeworkFour\\taskOne\\ByteIO.java");
+
+    }
+}
