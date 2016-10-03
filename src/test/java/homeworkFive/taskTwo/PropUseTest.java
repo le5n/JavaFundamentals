0c41d75685196ae102ef44b56d49608bb8bb243b
Lena <ellenageor@gmail.com>
2016-10-04 01:27:07 +0300

    exception test in task2 hw5 added

--- src/test/java/homeworkFive/taskTwo/PropUseTest.java
+++ src/test/java/homeworkFive/taskTwo/PropUseTest.java
@@ -3,13 +3,21 @@ package homeworkFive.taskTwo;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class PropUseTest {
+    PropUse propUse = new PropUse();
     @Test
     public void readKeyTest() throws Exception {
-        PropUse propUse = new PropUse();
         propUse.load("D:\\Программы\\JavaFundamentals\\src\\main\\resources\\PropUseFile.properties");
         String actualKey = propUse.findKey("KEY1");
         String expectedKey = "this is text in key1";
         Assert.assertEquals(expectedKey,actualKey);
+    }
+
+    @Test (expected = IOException.class)
+    public void exceptionTest() throws Exception{
+        propUse.load("D:\\Программы\\JavaFundamentals\\src\\main\\resources\\PropUseFile.properties");
+        propUse.findKey("not existing key");
     }
 }
